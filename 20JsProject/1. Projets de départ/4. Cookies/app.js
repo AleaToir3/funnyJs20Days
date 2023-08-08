fb4fdc3b0aa8ef961d4f563089c1a0ab5035f268
AleaToir3 <aleatoir3@gmail.com>
2023-08-08 16:14:45 +0100

    ajoute de create cookie et read cookie

--- 20JsProject/1. Projets de départ/4. Cookies/app.js
+++ 20JsProject/1. Projets de départ/4. Cookies/app.js
@@ -1,22 +1,24 @@
 const btnAffiche = document.getElementById("btnafficher");
 const monForm = document.querySelector('.monForm');
 const container = document.querySelector(".container")
+const inputs = document.querySelectorAll("input[type='text']")
 
 document.cookie = "coincoin=John Doe";
 
-btnAffiche.addEventListener('click', (e) => {
-    e.preventDefault();  
-    pop()
-
-    e.stopPropagation();  
-
-});
-
-monForm.addEventListener('click', (e) => {
+monForm.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log('=================valider===================');
+    createCookie(inputs)
+ 
+    pop()
 });
 
+
+btnAffiche.addEventListener('click', (e) => {
+    e.preventDefault();  
+    console.log(readCookie())
+    e.stopPropagation();  
+});
 
 function pop(){
     const div = document.createElement('div')
@@ -28,3 +30,20 @@ function pop(){
         popupValidation.innerHTML = ""
     },3000)    
 }
+
+function createCookie(inputs){
+    console.log("coucou")
+    let nom ;
+    let valeur;
+    inputs.forEach(input => {        
+        console.log(input)
+        if(input.name == "nom") nom = input.value
+        if(input.name == "valeur") valeur = input.value 
+    });    
+    return document.cookie = `${nom} = ${valeur}`
+}
+
+function readCookie(){
+    return document.cookie
+}
+// C R U D
