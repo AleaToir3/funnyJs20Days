ca10a7c7ca67053725cf38412f69490c871d5255
AleaToir3 <aleatoir3@gmail.com>
2023-08-10 00:17:15 +0100

    ajoute de delete et css

--- 20JsProject/1. Projets de départ/4. Cookies/app.js
+++ 20JsProject/1. Projets de départ/4. Cookies/app.js
@@ -5,64 +5,82 @@ const inputs = document.querySelectorAll("input[type='text']")
 
 monForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    inputs[0].value && inputs[1].value ? createCookie(inputs) : console.log('y a rien')
- 
-    pop()
+    
+    if (inputs[0].value && inputs[1].value) {
+        createCookie(inputs)
+        pop("ajout")
+    }
 });
 
 btnAffiche.addEventListener('click', (e) => {
-    e.preventDefault();  
+    e.preventDefault();
     readCookie()
-    e.stopPropagation();  
+    e.stopPropagation();
 });
 
-function pop(){
+function pop(status) {
     const div = document.createElement('div')
     div.classList.add("popupp")
-    div.textContent= "🍪Aouter ✅"
+    if(status == "ajout"){
+        div.textContent = "🍪Ajouter ✅"
+        div.style.background = "green"
+    }else{
+        div.textContent = "🍪Supprimer 🧹"
+        div.style.background = "red"
+    }
+    div.classList.add("apparait")
     popupValidation.appendChild(div)
-    div.classList.add("apparait")    
-    setTimeout(function(){
+
+    setTimeout(function () {
         popupValidation.innerHTML = ""
-    },3000)    
+    }, 3000)
 }
 
-function createCookie(inputs){
-    console.log("coucou")
-    let nom ;
+function createCookie(inputs) {
+    let nom;
     let valeur;
-    inputs.forEach(input => {        
-        if(input.name == "nom") nom = input.value
-        if(input.name == "valeur") valeur = input.value 
-    });    
+    inputs.forEach(input => {
+        if (input.name == "nom") nom = input.value
+        if (input.name == "valeur") valeur = input.value
+    });
+    inputs[0].value = ""
+    inputs[1].value = ""
     return document.cookie = `${nom} = ${valeur}`
 }
 
-function readCookie(){
-    const listCookies = document.cookie.split(";")
+function readCookie() {
+    const listCookies = document.cookie ? document.cookie.split(";") : null;
     let nom;
     let valeur;
-    listCookies.forEach((cookie,indexCookie) => {
-        // console.log('le cookie',cookie,"sonINDEX:",indexCookie)
-        cookie = cookie.split("=")
+    let deleteSpans = document.querySelectorAll(".spanDelete")
+ 
+    if(listCookies) {
+       console.log("coucou")
+        containerPostit.innerHTML = ""
+        listCookies.forEach((cookie, indexCookie) => {
+            // console.log('le cookie',cookie,"sonINDEX:",indexCookie)
+            cookie = cookie.split("=")
             containerPostit.innerHTML += `<div id="postit" name=${indexCookie}>
             <p>Nom : ${cookie[0]} </p>
             <p>Valeur : ${cookie[1]}</p>
             <span class="spanDelete">❌</span>
-          </div>`
-
-          const deleteSpans = document.querySelectorAll(".spanDelete")
-        //   console.log(deleteSpans)
-          deleteSpans.forEach((span)=>{
-            span.addEventListener('click',(e)=>{
-                // deleteCookie(listCookies,indexCookie)
+            </div>`
+        });
+        deleteSpans = document.querySelectorAll(".spanDelete")
+        deleteSpans.forEach((span) => {
+            span.addEventListener('click', (e) => {
+                deleteCookie(e, listCookies)
             })
 
-          }) 
-    }); 
+        })
+    }
 }
 
-function deleteCookie(listDecookie,indexAsupprimer){
-// console.log(listDecookie)
-// console.log(indexAsupprimer)
+function deleteCookie(e, listCookies) {
+    let id = e.target.parentNode.getAttribute('name')
+    let nameAsupprimer = listCookies[id].split("=")
+
+    document.cookie = `${nameAsupprimer[0]}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;`
+    e.target.parentNode.remove()
+    pop("supp")
 }
